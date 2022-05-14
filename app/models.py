c71470d88ac4889733ab77d740e9691c7b8620e2
brie <briememo95@gmail.com>
2022-05-14 12:40:31 +0300

    add class for pitches

--- app/models.py
+++ app/models.py
@@ -4,7 +4,6 @@ from flask_login import UserMixin, current_user, login_manager
 from datetime import datetime
 
 
-
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
@@ -57,3 +56,24 @@ class Pitches(db.Model):
 
     def __repr__(self):
         return f"Pitches {self.pitch}','{self.date}')"
+
+
+class Comments(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String(255))
+    date_posted = db.Column(db.DateTime(250), default=datetime.utcnow)
+    pitches_id = db.Column(db.Integer, db.ForeignKey("pitches.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comment(cls, id):
+        comments = Comments.query.filter_by(pitches_id=id).all()
+        return comments
+
+    def __repr__(self):
+        return f"Comments('{self.comment}', '{self.date_posted}')"
