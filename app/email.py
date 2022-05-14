03441c7a14291c36cda8202b491c3bb440cb45eb
brie <briememo95@gmail.com>
2022-05-14 13:56:22 +0300

    add email constructor to send emails

--- app/email.py
+++ app/email.py
@@ -1,2 +1,13 @@
-def mail_message():
-    return None
+from flask_mail import Message
+from flask import render_template
+from . import mail
+
+subject_pref = 'ELEVATOR PITCH-APP!'
+sender_email = 'briememo95@gmail.com'
+
+
+def mail_message(subject, template, to, **kwargs):
+    email = Message(subject, sender=sender_email, recipients=[to])
+    email.body = render_template(template + ".txt", **kwargs)
+    email.html = render_template(template + ".html", **kwargs)
+    mail.send(email)
